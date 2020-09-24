2ce2132ece496c4dd3b4953080576dc87b13c1e3
disa6302 <disa6302@colorado.edu>
2020-09-23 17:27:38 -0700

    Add env support

---
+++ producer-c/producer-cloudwatch-integ/init.sh
@@ -0,0 +1,4 @@
+export CANARY_STREAM_NAME=test 
+export CANARY_TYPE=realtime # Allowed values: realtime, offline
+export FRAGMENT_SIZE_IN_BYTES=1048576 # Preferrably multiples of 1024 bytes
+export CANARY_DURATION_IN_SECONDS=120
