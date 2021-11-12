b64592f95ab804e6dca758b134d9d464258bb9b3
Divya Sampath Kumar <disa6302@colorado.edu>
2021-11-12 09:32:41 -0800

    Branch off to include iot logging

--- canary/webrtc-c/jobs/orchestrator.groovy
+++ canary/webrtc-c/jobs/orchestrator.groovy
@@ -6,7 +6,7 @@ LONG_RUNNING_DURATION_IN_SECONDS = 0
 MIN_RETRY_DELAY_IN_SECONDS = 60
 COLD_STARTUP_DELAY_IN_SECONDS = 60 * 60
 GIT_URL = 'https://github.com/aws-samples/amazon-kinesis-video-streams-demos.git'
-GIT_HASH = 'new_release_1_5_1'
+GIT_HASH = 'enable_logging_new_release'
 COMMON_PARAMS = [
     string(name: 'AWS_KVS_LOG_LEVEL', value: "1"),
     string(name: 'MIN_RETRY_DELAY_IN_SECONDS', value: MIN_RETRY_DELAY_IN_SECONDS.toString()),
