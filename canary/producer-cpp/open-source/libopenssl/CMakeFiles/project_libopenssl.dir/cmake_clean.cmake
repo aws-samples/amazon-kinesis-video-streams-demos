31ad32cc41b3404320c404879890835a05ad8b46
stefankiesz <stefan.stefan4444@gmail.com>
2022-06-20 21:38:26 -0700

    Added opensource file

---
+++ canary/producer-cpp/open-source/libopenssl/CMakeFiles/project_libopenssl.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_libopenssl"
+  "CMakeFiles/project_libopenssl-complete"
+  "build/src/project_libopenssl-stamp/project_libopenssl-build"
+  "build/src/project_libopenssl-stamp/project_libopenssl-configure"
+  "build/src/project_libopenssl-stamp/project_libopenssl-download"
+  "build/src/project_libopenssl-stamp/project_libopenssl-install"
+  "build/src/project_libopenssl-stamp/project_libopenssl-mkdir"
+  "build/src/project_libopenssl-stamp/project_libopenssl-patch"
+  "build/src/project_libopenssl-stamp/project_libopenssl-test"
+  "build/src/project_libopenssl-stamp/project_libopenssl-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/project_libopenssl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ canary/producer-cpp/open-source/libopenssl/CMakeFiles/project_libopenssl.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for project_libopenssl.
+# This may be replaced when dependencies are built.
