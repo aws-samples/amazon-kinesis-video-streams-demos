4fbe80ffb9f4c91551b9548ace92d9b4ba88bd15
stefankiesz <stefan.stefan4444@gmail.com>
2022-06-27 12:57:53 -0700

    Cmake

---
+++ canary/producer-cpp/open-source/libkvsProducerCPP/CMakeFiles/libkvsProducerCPP-download.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libkvsProducerCPP-download"
+  "CMakeFiles/libkvsProducerCPP-download-complete"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-build"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-configure"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-download"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-install"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-mkdir"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-patch"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-test"
+  "build/src/libkvsProducerCPP-download-stamp/libkvsProducerCPP-download-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/libkvsProducerCPP-download.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ canary/producer-cpp/open-source/libkvsProducerCPP/CMakeFiles/libkvsProducerCPP-download.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for libkvsProducerCPP-download.
+# This may be replaced when dependencies are built.
