44e275432f9541161c6404f9a5626cc2a684eb4a
Nikunj Sarda <sardanik@amazon.com>
2023-03-13 17:17:19 -0700

    Git hash changed to new hash

--- canary/producer-cpp/jobs/orchestrator.groovy
+++ canary/producer-cpp/jobs/orchestrator.groovy
@@ -2,7 +2,7 @@ import jenkins.model.*
 
 WORKSPACE_PRODUCER="src"
 GIT_URL='https://github.com/aws-samples/amazon-kinesis-video-streams-demos.git'
-GIT_HASH='kvssink-canary-cpp-demos-latency'
+GIT_HASH='kvssink-canary-producer-cpp-demos'
 RUNNER_JOB_NAME_PREFIX = "producercpp-runner"
 // RUNNER_JOB_NAME_PREFIX = "producer-runner"
 
