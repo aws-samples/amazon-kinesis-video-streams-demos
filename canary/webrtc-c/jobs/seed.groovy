1e553d359222f21ba7970c1666f0a35924aa3cf0
Nikunj Sarda <sardanik@amazon.com>
2023-04-12 16:42:16 -0700

    Increased no of log retention

--- canary/webrtc-c/jobs/seed.groovy
+++ canary/webrtc-c/jobs/seed.groovy
@@ -6,7 +6,7 @@ import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.*;
 NAMESPACE="webrtc-canary"
 WORKSPACE="canary/webrtc-c"
 JOBS_DIR="$WORKSPACE/jobs"
-NUM_LOGS=1
+NUM_LOGS=9
 
 void approveSignatures(ArrayList<String> signatures) {
     scriptApproval = ScriptApproval.get()
