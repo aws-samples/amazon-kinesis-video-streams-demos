dba97a5784fb9886c02bfc59e0f14ca7e6798400
Nikunj Sarda <sardanik@amazon.com>
2023-04-17 09:51:02 -0700

    Trial

--- canary/webrtc-c/src/Config.cpp
+++ canary/webrtc-c/src/Config.cpp
@@ -193,10 +193,10 @@ STATUS Config::initWithEnvVars()
     CHK_STATUS(optenvBool(CANARY_USE_MEDIA_STORAGE, &useMediaStorage, FALSE));
     if(useMediaStorage.value == TRUE){
         if(channelName == "webrtc-canary-runner-0-WebrtcIngestionLongRunningStaticMbedTLS"){
-            storageStreamArn = "arn:aws:kinesisvideo:us-west-2:403080233248:stream/webrtc-canary-runner-0-WebrtcIngestionLongRunningStaticMbedTLS/1681748314423";
+            storageStreamArn = (PCHAR)"arn:aws:kinesisvideo:us-west-2:403080233248:stream/webrtc-canary-runner-0-WebrtcIngestionLongRunningStaticMbedTLS/1681748314423";
         }
         else {
-            storageStreamArn= "arn:aws:kinesisvideo:us-west-2:403080233248:stream/webrtc-canary-runner-1-WebrtcIngestionLongRunningStaticMbedTLS/1681748276351";
+            storageStreamArn= (PCHAR)"arn:aws:kinesisvideo:us-west-2:403080233248:stream/webrtc-canary-runner-1-WebrtcIngestionLongRunningStaticMbedTLS/1681748276351";
         }
 //        CHK_STATUS(mustenv(CANARY_STORAGE_STREAM_ARN, &storageStreamArn));
     }
