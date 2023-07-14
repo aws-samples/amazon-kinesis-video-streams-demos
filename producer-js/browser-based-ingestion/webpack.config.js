1e2e21563829076e9eaf4bdce83c9308784d373a
Jeremy Gunawan <jggunawa@amazon.com>
2023-07-14 10:39:11 -0700

    Crypto build

--- producer-js/browser-based-ingestion/webpack.config.js
+++ producer-js/browser-based-ingestion/webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    fallback: {
+      "crypto": false,
+      "crypto-browserify": require.resolve('crypto-browserify')
+    }
   }
 }
