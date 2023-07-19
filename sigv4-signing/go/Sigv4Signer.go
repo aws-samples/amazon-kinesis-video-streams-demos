098b5afe03e1cdea3c1e44a62885488cf005b86a
Jeremy Gunawan <jggunawa@amazon.com>
2023-07-19 11:50:31 -0700

    Add sigv4 signer sample

---
+++ sigv4-signing/go/Sigv4Signer.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"io"
+	"log"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"os/signal"
+	"sort"
+	"strings"
+	"time"
+)
+
+const (
+	ALGORITHM_AWS4_HMAC_SHA_256 = "AWS4-HMAC-SHA256"
+	AWS4_REQUEST_TYPE           = "aws4_request"
+	SERVICE                     = "kinesisvideo"
+	X_AMZ_ALGORITHM             = "X-Amz-Algorithm"
+	X_AMZ_CREDENTIAL            = "X-Amz-Credential"
+	X_AMZ_DATE                  = "X-Amz-Date"
+	X_AMZ_EXPIRES               = "X-Amz-Expires"
+	X_AMZ_SECURITY_TOKEN        = "X-Amz-Security-Token"
+	X_AMZ_SIGNATURE             = "X-Amz-Signature"
+	X_AMZ_SIGNED_HEADERS        = "X-Amz-SignedHeaders"
+	NEW_LINE_DELIMITER          = "\n"
+	DATE_PATTERN                = "20060102"
+	TIME_PATTERN                = "20060102T150405Z"
+	METHOD                      = "GET"
+	SIGNED_HEADERS              = "host"
+)
+
+type AwsV4Signer struct{}
+
+// SigV4 Signer sample reference for the Kinesis Video Streams WebRTC WebSocket connections
+// Uses Gorilla WebSockets Go Module
+
+func main() {
+
+	////////////////////////////////////////////
+	// ***Sample connection test:*** - fill in the variables:
+
+	// Connect as Viewer URL - GetSignalingChannelEndpoint + Query Parameters Channel ARN as X-Amz-ChannelARN + Client Id as X-Amz-ClientId
+	uri, _ := url.Parse("wss://<Your GetSignalingEndpoint response hostname>?X-Amz-ChannelARN=<YourChannelARN>&X-Amz-ClientId=<YourClientId>")
+
+	// WebSocket Method WSS plus Hostname
+	wssUri, _ := url.Parse("wss://<Your GetSignalingEndpoint Response hostname>")
+
+	// Test Credentials for sample connection verification. SessionToken can be empty string if using non-temporary credentials.
+	accessKey := "YourAccessKey"
+	secretKey := "YourSecretKey"
+	sessionToken := "YourSessionToken"
+
+	// AWS Region (e.g. us-west-2)
+	region := "YourChannelRegion"
+
+	////////////////////////////////////////////
+
+
+	// Compute SigV4 signing
+	// Ref: https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-query-string-auth.html
+	// https://github.com/awslabs/amazon-kinesis-video-streams-webrtc-sdk-android/blob/master/src/main/java/com/amazonaws/kinesisvideo/utils/AwsV4Signer.java
+
+	signedUri := AwsV4Signer{}.sign(uri, accessKey, secretKey, sessionToken, wssUri, region)
+	println("Go SignedURL log for verifications (e.g. using wscat ) =", signedUri.String())
+
+	// Connect to the SigV4 signed Endpoint
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	c, resp, err := websocket.DefaultDialer.Dial(signedUri.String(), nil)
+
+	// Print the responses from the connection
+	println("Response code=", resp.StatusCode)
+	if err != nil {
+		log.Fatalln("Error in connection: ", err)
+	}
+
+	b, errBodyResp := io.ReadAll(resp.Body)
+	if errBodyResp != nil {
+		log.Fatalln("Error in reading response body: ", errBodyResp)
+	}
+
+	fmt.Println("Response body", string(b))
+
+	body, errResp := httputil.DumpResponse(resp, true)
+	if errResp != nil {
+		log.Fatal("Error printing response body:", err)
+	}
+
+	fmt.Println(string(body))
+
+	defer c.Close()
+
+}
+
+func (signer AwsV4Signer) sign(uri *url.URL, accessKey, secretKey, sessionToken string, wssURI *url.URL, region string) *url.URL {
+
+	dateMilli := time.Now().UnixMilli()
+	amzDate := getTimeStamp(dateMilli)
+	datestamp := getDateStamp(dateMilli)
+
+	queryParamsMap := buildQueryParamsMap(uri, accessKey, sessionToken, region, amzDate, datestamp)
+	canonicalQuerystring := getCanonicalizedQueryString(queryParamsMap)
+	canonicalRequest := getCanonicalRequest(uri, canonicalQuerystring)
+	stringToSign := signString(amzDate, createCredentialScope(region, datestamp), canonicalRequest)
+
+	signatureKey := getSignatureKey(secretKey, datestamp, region, SERVICE)
+	signature := hex.EncodeToString(hmacSha256(stringToSign, signatureKey))
+
+	signedCanonicalQueryString := canonicalQuerystring + "&" + X_AMZ_SIGNATURE + "=" + signature
+
+	signedURI, err := url.Parse(wssURI.Scheme + "://" + wssURI.Host + getCanonicalURI(uri) + "?" + signedCanonicalQueryString)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return signedURI
+}
+
+func buildQueryParamsMap(uri *url.URL, accessKey, sessionToken, region, amzDate, datestamp string) map[string]string {
+
+	xAmzCredential := urlEncode(accessKey + "/" + createCredentialScope(region, datestamp))
+
+	queryParamsMap := map[string]string{
+		X_AMZ_ALGORITHM:  ALGORITHM_AWS4_HMAC_SHA_256,
+		X_AMZ_CREDENTIAL: xAmzCredential,
+		X_AMZ_DATE:       amzDate,
+		X_AMZ_EXPIRES:    "299",
+	}
+
+	if sessionToken != "" {
+		queryParamsMap[X_AMZ_SECURITY_TOKEN] = urlEncode(sessionToken)
+	}
+
+	queryParamsMap[X_AMZ_SIGNED_HEADERS] = SIGNED_HEADERS
+
+	if uri.RawQuery != "" {
+		params := strings.Split(uri.RawQuery, "&")
+		for _, param := range params {
+			index := strings.Index(param, "=")
+			if index > 0 {
+				queryParamsMap[param[:index]] = urlEncode(param[index+1:])
+			}
+		}
+	}
+
+	println("\n")
+
+	return queryParamsMap
+}
+
+func createCredentialScope(region, datestamp string) string {
+	ret := strings.Join([]string{datestamp, region, SERVICE, AWS4_REQUEST_TYPE}, "/")
+	return ret
+}
+
+func getCanonicalRequest(uri *url.URL, canonicalQuerystring string) string {
+	// https://pkg.go.dev/crypto/sha256
+	payloadHash := sha256.New().Sum([]byte(""))
+	canonicalUri := getCanonicalURI(uri)
+	canonicalHeaders := "host:" + uri.Host + NEW_LINE_DELIMITER
+	canonicalRequest := strings.Join([]string{METHOD, canonicalUri, canonicalQuerystring, canonicalHeaders, SIGNED_HEADERS, hex.EncodeToString(payloadHash)}, NEW_LINE_DELIMITER)
+
+	return canonicalRequest
+}
+
+func getCanonicalURI(uri *url.URL) string {
+	if uri.Path == "" {
+		return "/"
+	}
+	return uri.Path
+}
+
+func signString(amzDate, credentialScope, canonicalRequest string) string {
+	// https://pkg.go.dev/crypto/sha256#Sum256
+	// Sum256 returns the SHA256 checksum of the data.
+	result := sha256.Sum256([]byte(canonicalRequest))
+	// Slice to make it to variable size for encoding
+	result2 := hex.EncodeToString(result[:])
+
+	stringToSign := strings.Join([]string{ALGORITHM_AWS4_HMAC_SHA_256, amzDate, credentialScope, result2},
+		NEW_LINE_DELIMITER)
+	return stringToSign
+}
+
+func urlEncode(str string) string {
+	ret := url.QueryEscape(str)
+	return ret
+}
+
+func hmacSha256(data string, key []byte) []byte {
+	mac := hmac.New(sha256.New, key)
+	mac.Write([]byte(data))
+	ret := mac.Sum(nil)
+	return ret
+}
+
+func getSignatureKey(key, dateStamp, regionName, serviceName string) []byte {
+	kSecret := []byte("AWS4" + key)
+	kDate := hmacSha256(dateStamp, kSecret)
+	kRegion := hmacSha256(regionName, kDate)
+	kService := hmacSha256(serviceName, kRegion)
+	ret := hmacSha256(AWS4_REQUEST_TYPE, kService)
+	return ret
+}
+
+func getTimeStamp(dateMilli int64) string {
+	ret := time.UnixMilli(dateMilli).UTC().Format(TIME_PATTERN)
+	return ret
+}
+
+func getDateStamp(dateMilli int64) string {
+	ret := time.UnixMilli(dateMilli).UTC().Format(DATE_PATTERN)
+	return ret
+}
+
+func getCanonicalizedQueryString(queryParamsMap map[string]string) string {
+	var queryKeys []string
+	for key := range queryParamsMap {
+		queryKeys = append(queryKeys, key)
+	}
+	sort.Strings(queryKeys)
+
+	var builder strings.Builder
+	for i, key := range queryKeys {
+		builder.WriteString(key + "=" + queryParamsMap[key])
+		if i < len(queryKeys)-1 {
+			builder.WriteString("&")
+		}
+	}
+
+	return builder.String()
+}
---
+++ sigv4-signing/java/SigV4Signer.java/src/main/java/org/amazonaws/kinesisvideo/AwsV4Signer.java
@@ -0,0 +1,304 @@
+package org.amazonaws.kinesisvideo;
+
+import com.amazonaws.util.BinaryUtils;
+import com.google.common.collect.ImmutableMap;
+import org.glassfish.tyrus.client.ClientManager;
+import org.glassfish.tyrus.client.ClientProperties;
+import org.glassfish.tyrus.client.SslEngineConfigurator;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.websocket.Endpoint;
+import javax.websocket.EndpointConfig;
+import javax.websocket.Session;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.TimeZone;
+
+import static com.google.common.hash.Hashing.sha256;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
+
+@SuppressWarnings({"SpellCheckingInspection", "SameParameterValue"})
+public class AwsV4Signer {
+
+    private static final String ALGORITHM_AWS4_HMAC_SHA_256 = "AWS4-HMAC-SHA256";
+    private static final String AWS4_REQUEST_TYPE = "aws4_request";
+    private static final String SERVICE = "kinesisvideo";
+    private static final String X_AMZ_ALGORITHM = "X-Amz-Algorithm";
+    private static final String X_AMZ_CREDENTIAL = "X-Amz-Credential";
+    private static final String X_AMZ_DATE = "X-Amz-Date";
+    private static final String X_AMZ_EXPIRES = "X-Amz-Expires";
+    private static final String X_AMZ_SECURITY_TOKEN = "X-Amz-Security-Token";
+    private static final String X_AMZ_SIGNATURE = "X-Amz-Signature";
+    private static final String X_AMZ_SIGNED_HEADERS = "X-Amz-SignedHeaders";
+    private static final String NEW_LINE_DELIMITER = "\n";
+    private static final String DATE_PATTERN = "yyyyMMdd";
+    private static final String TIME_PATTERN = "yyyyMMdd'T'HHmmss'Z'";
+
+    private static final String METHOD = "GET";
+    private static final String SIGNED_HEADERS = "host";
+
+    // SigV4 Signer sample reference for the Kinesis Video Streams WebRTC WebSocket connections
+    // It will generate a sigv4 url given valid parameters, then connect to it using Tyrus WebSocket client
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
+
+        /////////////////////////////////////////
+
+        // URI to sign.
+        // Connect as Viewer URL - GetSignalingChannelEndpoint + Query Parameters: Channel ARN as X-Amz-ChannelARN & Client Id as X-Amz-ClientId
+        // For example: wss://v-a1b2c3d4.kinesisvideo.us-west-2.amazonaws.com?X-Amz-ChannelARN=arn:aws:kinesisvideo:us-west-2:123456789012:channel/demo-channel/1234567890123&X-Amz-ClientId=d7d1c6e2-9cb0-4d61-bea9-ecb3d3816557
+        final URI uri = new URI("");
+
+        // AWS Credentials. Session Token should be empty string if non-temporary credentials are used.
+        final String accessKey = "";
+        final String secretKey = "";
+        final String sessionToken = "";
+
+        // Secure WebSocket method "wss" plus hostname obtained from GetSignalingChannelEndpoint
+        // For example: wss://v-a1b2c3d4.kinesisvideo.us-west-2.amazonaws.com
+        final URI wssUri = new URI("");
+
+        // AWS Region. For example, us-west-2
+        final String region = "us-west-2";
+
+        /////////////////////////////////////////
+
+        final ClientManager client = ClientManager.createClient();
+        final SSLContext ctx = SSLContext.getInstance("TLS");
+        final TrustManager[] trustManagers = {new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                // do not check anything
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                // do not check anything
+            }
+        }};
+        ctx.init(null, trustManagers, null);
+        final SslEngineConfigurator sslEngineConfigurator = new SslEngineConfigurator(ctx);
+        sslEngineConfigurator.setHostVerificationEnabled(false);
+        client.getProperties().put(ClientProperties.SSL_ENGINE_CONFIGURATOR, sslEngineConfigurator);
+        client.getProperties().put(ClientProperties.LOG_HTTP_UPGRADE, true);
+
+        final URI signedUri = sign(uri, accessKey, secretKey, sessionToken, wssUri, region);
+
+        try (final Session session = client.connectToServer(new Endpoint() {
+            @Override
+            public void onOpen(Session session, EndpointConfig config) {
+                System.out.println("WebSocket opened!");
+            }
+        }, null, URI.create(signedUri.toString()))) {
+            System.out.println("Success!");
+        } catch (final Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    // Guide - https://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
+    // Implementation based on https://docs.aws.amazon.com/general/latest/gr/sigv4-signed-request-examples.html#sig-v4-examples-get-query-string
+    public static URI sign(final URI uri, final String accessKey, final String secretKey,
+                           final String sessionToken, final URI wssUri, final String region) {
+
+        final long dateMilli = new Date().getTime();
+        final String amzDate = getTimeStamp(dateMilli);
+        final String datestamp = getDateStamp(dateMilli);
+
+        final Map<String, String> queryParamsMap = buildQueryParamsMap(uri, accessKey, sessionToken, region, amzDate, datestamp);
+        final String canonicalQuerystring = getCanonicalizedQueryString(queryParamsMap);
+        final String canonicalRequest = getCanonicalRequest(uri, canonicalQuerystring);
+        final String stringToSign = signString(amzDate, createCredentialScope(region, datestamp), canonicalRequest);
+        final byte[] signatureKey = getSignatureKey(secretKey, datestamp, region, SERVICE);
+        final String signature = BinaryUtils.toHex(hmacSha256(stringToSign, signatureKey));
+
+        final String signedCanonicalQueryString = canonicalQuerystring + "&" + X_AMZ_SIGNATURE + "=" + signature;
+
+        URI uriResult = null;
+        try {
+            uriResult = new URI(wssUri.getScheme(),
+                    wssUri.getRawAuthority(),
+                    getCanonicalUri(uri),
+                    signedCanonicalQueryString, // Note: URI constructor will encode non-URI characters in this String.
+                    null);
+        } catch (final URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return uriResult;
+    }
+
+    private static Map<String, String> buildQueryParamsMap(URI uri, String accessKey, String sessionToken, String region, String amzDate, String datestamp) {
+        final ImmutableMap.Builder<String, String> queryParamsBuilder = ImmutableMap.<String, String>builder()
+                .put(X_AMZ_ALGORITHM, ALGORITHM_AWS4_HMAC_SHA_256)
+                .put(X_AMZ_CREDENTIAL, urlEncode(accessKey + "/" + createCredentialScope(region, datestamp)))
+                .put(X_AMZ_DATE, amzDate)
+                .put(X_AMZ_EXPIRES, "299")
+                .put(X_AMZ_SIGNED_HEADERS, SIGNED_HEADERS);
+
+        if (isNotEmpty(sessionToken)) {
+            queryParamsBuilder.put(X_AMZ_SECURITY_TOKEN, urlEncode(sessionToken));
+        }
+
+        if (isNotEmpty(uri.getQuery())) {
+            final String[] params = uri.getQuery().split("&");
+            for (final String param : params) {
+                final int index = param.indexOf('=');
+                if (index > 0) {
+                    queryParamsBuilder.put(param.substring(0, index), urlEncode(param.substring(index + 1)));
+                }
+            }
+        }
+        return queryParamsBuilder.build();
+    }
+
+    private static String createCredentialScope(String region, String datestamp) {
+        return new StringJoiner("/").add(datestamp).add(region).add(SERVICE).add(AWS4_REQUEST_TYPE).toString();
+    }
+
+    private static String getCanonicalRequest(URI uri, String canonicalQuerystring) {
+        final String payloadHash = sha256().hashString(EMPTY, UTF_8).toString();
+        final String canonicalUri = getCanonicalUri(uri);
+        final String canonicalHeaders = "host:" + uri.getHost() + NEW_LINE_DELIMITER;
+        final String canonicalRequest = new StringJoiner(NEW_LINE_DELIMITER).add(METHOD)
+                .add(canonicalUri)
+                .add(canonicalQuerystring)
+                .add(canonicalHeaders)
+                .add(SIGNED_HEADERS)
+                .add(payloadHash)
+                .toString();
+
+        return canonicalRequest;
+    }
+
+    private static String getCanonicalUri(URI uri) {
+        return isEmpty(uri.getPath()) ? "/" : uri.getPath();
+    }
+
+    private static String signString(String amzDate, String credentialScope, String canonicalRequest) {
+        final String stringToSign = new StringJoiner(NEW_LINE_DELIMITER).add(ALGORITHM_AWS4_HMAC_SHA_256)
+                .add(amzDate)
+                .add(credentialScope)
+                .add(sha256().hashString(canonicalRequest, UTF_8).toString()) // checksum
+                .toString();
+        return stringToSign;
+    }
+
+    private static String urlEncode(final String str) {
+        try {
+            return URLEncoder.encode(str, UTF_8.name());
+        } catch (final UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+
+    //  https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-java
+    private static byte[] hmacSha256(final String data, final byte[] key) {
+        final String algorithm = "HmacSHA256";
+        final Mac mac;
+        try {
+            mac = Mac.getInstance(algorithm);
+            mac.init(new SecretKeySpec(key, algorithm));
+            return mac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        } catch (final NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+
+    //   https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-java
+    private static byte[] getSignatureKey(
+            final String key,
+            final String dateStamp,
+            final String regionName,
+            final String serviceName) {
+        final byte[] kSecret = ("AWS4" + key).getBytes(StandardCharsets.UTF_8);
+        final byte[] kDate = hmacSha256(dateStamp, kSecret);
+        final byte[] kRegion = hmacSha256(regionName, kDate);
+        final byte[] kService = hmacSha256(serviceName, kRegion);
+        return hmacSha256(AWS4_REQUEST_TYPE, kService);
+    }
+
+    private static String getTimeStamp(long dateMilli) {
+        return format(TIME_PATTERN, new Date(dateMilli));
+    }
+
+    private static String getDateStamp(long dateMilli) {
+        return format(DATE_PATTERN, new Date(dateMilli));
+    }
+
+    private static String getCanonicalizedQueryString(Map<String, String> queryParamsMap) {
+        final List<String> queryKeys = new ArrayList<>(queryParamsMap.keySet());
+        Collections.sort(queryKeys);
+
+        final StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < queryKeys.size(); i++) {
+            builder.append(queryKeys.get(i)).append("=").append(queryParamsMap.get(queryKeys.get(i)));
+            if (queryKeys.size() - 1 > i) {
+                builder.append("&");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    private static String format(String pattern, Date date) {
+        return getSimpleDateFormat(pattern).get().format(date);
+    }
+
+    private static final Map<String, ThreadLocal<SimpleDateFormat>> SDF_MAP = new HashMap<>();
+
+    private static final TimeZone GMT_TIMEZONE = TimeZone.getTimeZone("GMT");
+
+    private static ThreadLocal<SimpleDateFormat> getSimpleDateFormat(final String pattern) {
+        ThreadLocal<SimpleDateFormat> sdf = SDF_MAP.get(pattern);
+        if (sdf == null) {
+            synchronized (SDF_MAP) {
+                sdf = SDF_MAP.get(pattern);
+                if (sdf == null) {
+                    sdf = new ThreadLocal<SimpleDateFormat>() {
+                        @Override
+                        protected SimpleDateFormat initialValue() {
+                            final SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.US);
+                            sdf.setTimeZone(GMT_TIMEZONE);
+                            sdf.setLenient(false);
+                            return sdf;
+                        }
+                    };
+                    SDF_MAP.put(pattern, sdf);
+                }
+            }
+        }
+        return sdf;
+    }
+}
