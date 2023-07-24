367b73c0287576aadc1a72c9cd1e9757144af8f9
stefankiesz <stefan.stefan4444@gmail.com>
2023-07-24 18:34:35 +0000

    Undo customFrame changes for WebRTC, start consumer-end Canary

--- canary/consumer-java/src/main/java/com/amazon/kinesis/video/canary/consumer/ProducerSdkCanaryConsumer.java
+++ canary/consumer-java/src/main/java/com/amazon/kinesis/video/canary/consumer/ProducerSdkCanaryConsumer.java
@@ -15,6 +15,10 @@ import com.amazonaws.services.kinesisvideo.AmazonKinesisVideo;
 import com.amazonaws.services.kinesisvideo.AmazonKinesisVideoClientBuilder;
 import com.amazonaws.services.kinesisvideo.model.StartSelector;
 import com.amazonaws.services.kinesisvideo.model.StartSelectorType;
+
+import com.amazonaws.services.kinesisvideo.model.APIName;
+import com.amazonaws.services.kinesisvideo.model.GetDataEndpointRequest;
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -25,14 +29,22 @@ import java.util.TimerTask;
 
 @Slf4j
 public class ProducerSdkCanaryConsumer {
+
+    private static void getIntervalMetrics(){
+        System.out.println("10 sec have passed...");
+        
+    }
+
     public static void main(final String[] args) throws Exception {
         String streamNamePrefix = System.getenv("CANARY_STREAM_NAME");
         String canaryType = System.getenv("CANARY_TYPE");
         String canaryFragmentSizeStr = System.getenv("FRAGMENT_SIZE_IN_BYTES");
         String canaryLabel = System.getenv("CANARY_LABEL");
         String region = System.getenv("AWS_DEFAULT_REGION");
-        final String streamName = String.format("%s-%s-%s", streamNamePrefix, canaryType,
-                    canaryLabel);
+
+        // TODO: Revert back to adding the Canary descriptions to stream name, removed for testing
+        final String streamName = streamNamePrefix;
+
         Integer canaryRunTime = Integer.parseInt(System.getenv("CANARY_DURATION_IN_SECONDS"));
         log.info("Stream name {}", streamName);
 
@@ -64,15 +76,28 @@ public class ProducerSdkCanaryConsumer {
                 amazonKinesisVideo,
                 consumerFactory);
 
+        Timer timer = new Timer("Timer");
         TimerTask task = new TimerTask() {
             public void run() {
                 getMediaWorker.stop();
             }
         };
-        Timer timer = new Timer("Timer");
-        
         long delay = canaryRunTime * 1000;
         timer.schedule(task, delay);
+
+
+        final GetDataEndpointRequest dataEndpointRequest = new GetDataEndpointRequest()
+                .withAPIName(APIName.LIST_FRAGMENTS).withStreamName(streamName);
+        final String dataEndpoint = amazonKinesisVideo.getDataEndpoint(dataEndpointRequest).getDataEndpoint();
+
+        Timer intervalMetricsTimer = new Timer("IntervalMetricsTimer");
+        TimerTask intervalMetricsTask = new TimerTask() {
+            public void run() {
+                getIntervalMetrics();
+            }
+        };
+        intervalMetricsTimer.scheduleAtFixedRate(intervalMetricsTask, 0, 10000); // delay of 0 ms at an interval of 10,000 ms
+
 
         getMediaWorker.run();
         timer.cancel(); 
