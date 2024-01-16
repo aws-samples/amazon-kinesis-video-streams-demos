43e3cf4787fed461156e4144a21128e947dbf435
Stefan Kieszkowski <stefan.stefan4444@gmail.com>
2024-01-16 19:09:20 +0000

    Change orchestrator to master branch

--- canary/webrtc-c/jobs/orchestrator.groovy
+++ canary/webrtc-c/jobs/orchestrator.groovy
@@ -13,7 +13,7 @@ STORAGE_EXTENDED_DURATION_IN_SECONDS = 43200 // 12 hr
 MIN_RETRY_DELAY_IN_SECONDS = 60
 COLD_STARTUP_DELAY_IN_SECONDS = 60 * 60
 GIT_URL = 'https://github.com/aws-samples/amazon-kinesis-video-streams-demos.git'
-GIT_HASH = 'sample-media-server-canary' // TODO: Revert to 'master'
+GIT_HASH = 'master'
 COMMON_PARAMS = [
     string(name: 'AWS_KVS_LOG_LEVEL', value: "2"),
     string(name: 'MIN_RETRY_DELAY_IN_SECONDS', value: MIN_RETRY_DELAY_IN_SECONDS.toString()),
