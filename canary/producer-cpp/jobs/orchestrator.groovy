9ec6c630fcbe08994203d8dcf4e19432d3f2b2e6
Divya Sampath Kumar <disa6302@colorado.edu>
2024-03-12 11:40:51 -0700

    Master branch

--- canary/producer-cpp/jobs/orchestrator.groovy
+++ canary/producer-cpp/jobs/orchestrator.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.*
 
 GIT_URL='https://github.com/aws-samples/amazon-kinesis-video-streams-demos.git'
-GIT_HASH='prod-commit-update'
+GIT_HASH='master'
 RUNNER_JOB_NAME_PREFIX = "producercpp-runner"
 
 LONG_RUN_DURATION_IN_SECONDS = 12 * 60 * 60
