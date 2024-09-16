2b4c2723a142bf8e87dd1f8a46ab17ee313a784c
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2024-09-16 12:18:51 -0700

    Update orchestrator.groovy to 1.9.1-benchmark

--- canary/webrtc-c/jobs/orchestrator.groovy
+++ canary/webrtc-c/jobs/orchestrator.groovy
@@ -13,7 +13,7 @@ STORAGE_EXTENDED_DURATION_IN_SECONDS = 43200 // 12 hr
 MIN_RETRY_DELAY_IN_SECONDS = 60
 COLD_STARTUP_DELAY_IN_SECONDS = 60 * 60
 GIT_URL = 'https://github.com/aws-samples/amazon-kinesis-video-streams-demos.git'
-GIT_HASH = 'wc-1.10.3'
+GIT_HASH = '1.9.1-benchmark'
 COMMON_PARAMS = [
     string(name: 'AWS_KVS_LOG_LEVEL', value: "2"),
     string(name: 'DEBUG_LOG_SDP', value: "TRUE"),
