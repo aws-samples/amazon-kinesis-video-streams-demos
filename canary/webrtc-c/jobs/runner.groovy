2350be6ef4ce71df58f606372a4f3d3cb11e870b
Stefan Kieszkowski <stefan.stefan4444@gmail.com>
2025-02-01 19:53:47 +0000

    more

--- canary/webrtc-c/jobs/runner.groovy
+++ canary/webrtc-c/jobs/runner.groovy
@@ -293,17 +293,17 @@ pipeline {
                     echo "Finished performing assume-role call"
                     echo "Assume Role Output: ${assumeRoleOutput}"
 
-                    def assumeRoleJson = readJSON text: assumeRoleOutput
+                    // def assumeRoleJson = readJSON text: assumeRoleOutput
 
-                    echo "Parsed JSON: ${assumeRoleJson}"
-                    env.ASSUME_ROLE_OUTPUT = assumeRoleJson
-                    echo "ASSUME_ROLE_OUTPUT: ${env.ASSUME_ROLE_OUTPUT}"
+                    // echo "Parsed JSON: ${assumeRoleJson}"
+                    // env.ASSUME_ROLE_OUTPUT = assumeRoleJson
+                    // echo "ASSUME_ROLE_OUTPUT: ${env.ASSUME_ROLE_OUTPUT}"
 
-                    // env.AWS_ACCESS_KEY_ID = assumeRoleJson.Credentials.AccessKeyId
-                    // env.AWS_SECRET_ACCESS_KEY = assumeRoleJson.Credentials.SecretAccessKey
-                    // env.AWS_SESSION_TOKEN = assumeRoleJson.Credentials.SessionToken
+                    env.AWS_ACCESS_KEY_ID = assumeRoleOutput.Credentials.AccessKeyId
+                    env.AWS_SECRET_ACCESS_KEY = assumeRoleOutput.Credentials.SecretAccessKey
+                    env.AWS_SESSION_TOKEN = assumeRoleOutput.Credentials.SessionToken
 
-                    // echo "AWS_ACCESS_KEY_ID: ${env.AWS_ACCESS_KEY_ID}"
+                    echo "AWS_ACCESS_KEY_ID: ${env.AWS_ACCESS_KEY_ID}"
                 }
                 
                 // echo 'CANARY_STS_ROLE_ARN: ${env.CANARY_STS_ROLE_ARN}'
