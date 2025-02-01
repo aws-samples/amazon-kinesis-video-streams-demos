003a7fc79070847a8dbbcb12e8337e3ffe19ceec
Stefan Kieszkowski <stefan.stefan4444@gmail.com>
2025-02-01 01:28:46 +0000

    Fix runner script syntax

--- canary/webrtc-c/jobs/runner.groovy
+++ canary/webrtc-c/jobs/runner.groovy
@@ -285,23 +285,25 @@ pipeline {
 
     stages {
         stage('Fetch and export STS credentials') {
-            script {
-                def assumeRoleOutput = sh(
-                    script: '''
-                        unset AWS_ACCESS_KEY_ID \
-                        unset AWS_SECRET_ACCESS_KEY \
-                        aws sts assume-role \
-                            --role-arn arn:aws:iam::123456789012:role/xaccounts3access \
-                            --role-session-name s3-access-example
-                    ''',
-                    returnStdout: true
-                ).trim()
+            steps {
+                script {
+                    def assumeRoleOutput = sh(
+                        script: '''
+                            unset AWS_ACCESS_KEY_ID \
+                            unset AWS_SECRET_ACCESS_KEY \
+                            aws sts assume-role \
+                                --role-arn arn:aws:iam::123456789012:role/xaccounts3access \
+                                --role-session-name s3-access-example
+                        ''',
+                        returnStdout: true
+                    ).trim()
 
-                def json = readJSON text: assumeRoleOutput
-                env.AWS_ACCESS_KEY_ID = json.Credentials.AccessKeyId
-                env.AWS_SECRET_ACCESS_KEY = json.Credentials.SecretAccessKey
-                env.AWS_SESSION_TOKEN = json.Credentials.SessionToken
-            }
+                    def json = readJSON text: assumeRoleOutput
+                    env.AWS_ACCESS_KEY_ID = json.Credentials.AccessKeyId
+                    env.AWS_SECRET_ACCESS_KEY = json.Credentials.SecretAccessKey
+                    env.AWS_SESSION_TOKEN = json.Credentials.SessionToken
+                }    
+            }                                       
         }
 
         stage('Set build description') {
