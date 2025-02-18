0447c1a12b6b16f0416b6e42c98a430972a63d10
Stefan Kieszkowski <85728496+stefankiesz@users.noreply.github.com>
2025-02-17 16:23:41 -0800

    Fix

--- canary/webrtc-c/jobs/runner.groovy
+++ canary/webrtc-c/jobs/runner.groovy
@@ -270,15 +270,18 @@ pipeline {
         string(name: 'GIT_HASH')
         booleanParam(name: 'FIRST_ITERATION', defaultValue: true)
     }
-
+    
     stages {
         stage('Fetch STS credentials and export to env vars') {
             steps {
                 script {
                     def roleArn = credentials('CANARY_STS_ROLE_ARN')
 
-                    // TODO: Add back the .trim if it works.
-                    def assumeRoleOutput = sh(script: 'aws sts assume-role --role-arn ' + roleArn + ' --role-session-name roleSessionName --duration-seconds 43200 --output json', returnStdout: true).trim()
+                    // Export the role ARN to env var to avoid string interpolation to follow Jenkins security guidelines.
+                    def assumeRoleOutput = sh(script: '''
+                        export AWS_KVS_STS_ROLE_ARN=${roleArn}
+                        aws sts assume-role --role-arn $AWS_KVS_STS_ROLE_ARN --role-session-name roleSessionName --duration-seconds 43200 --output json''',
+                        returnStdout: true).trim()
                     def assumeRoleJson = readJSON text: assumeRoleOutput
 
                     env.AWS_ACCESS_KEY_ID = assumeRoleJson.Credentials.AccessKeyId
