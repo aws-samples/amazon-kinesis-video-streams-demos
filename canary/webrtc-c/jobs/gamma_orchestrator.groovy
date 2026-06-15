d6b56474f9d9fce80b943df87f14344d79b245ab
Yuqi Huang <yuuqih@amazon.com>
2026-06-14 22:08:56 -0700

    update GIT HASH to stability-fix

--- canary/webrtc-c/jobs/gamma_orchestrator.groovy
+++ canary/webrtc-c/jobs/gamma_orchestrator.groovy
@@ -18,7 +18,7 @@
  */
 
 GIT_URL = 'https://github.com/aws-samples/amazon-kinesis-video-streams-demos.git'
-GIT_HASH = 'metric-implementation'
+GIT_HASH = 'stability-fix'
 
 // Dedicated gamma runner job name
 GAMMA_RUNNER_JOB = "webrtc-gamma-runner"
